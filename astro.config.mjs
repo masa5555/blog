0bca3859f9003236a3aa3be2e574aebafd1e0ad5
masa5555 <masa57887@gmail.com>
2024-01-01 14:16:01 +0900

    Update site base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://masa5555.github.io",
-  base: "/blog",
+  site: "https://masa5555.github.io/blog",
   integrations: [mdx(), sitemap()],
 });
