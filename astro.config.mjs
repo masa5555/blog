5bf6016ee9ed3979850c8c65c1a6e535637f96ad
masa5555 <masa57887@gmail.com>
2025-01-02 14:24:26 +0900

    RSSが表示されるようにした

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://masa5555.github.io",
   base: "/blog",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), icon()]
 });
